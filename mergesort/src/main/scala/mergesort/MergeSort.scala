662a268e8e63f791f7913535ace290848eec625c
before30 <before30@gmail.com>
2016-07-20 20:39:27 +0900

    add main

---
+++ mergesort/src/main/scala/mergesort/MergeSort.scala
@@ -0,0 +1,23 @@
+package mergesort
+
+/**
+  * User: before30 
+  * Date: 2016. 7. 20.
+  * Time: 오후 8:32
+  */
+object MergeSort {
+
+  def sort(input: List[Int]): List[Int] = {
+    input
+  }
+
+  def merge(xs: List[Int], ys: List[Int]): List[Int] = {
+    ???
+  }
+
+  def main(args: Array[String]) {
+    println("Application starts.")
+    val values: List[Int] = List(5, 11, 8, 4, 2)
+    println(sort(values))
+  }
+}
